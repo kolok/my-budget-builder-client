a15053f87fe172d6cb9e8b914b94181bbd025d8b
Igor Cuckovic <cuckovic@gmail.com>
2019-09-15 19:20:18 +0200

    StackedBarChart add margin for TODAY

--- src/components/charts/StackedBarChart.vue
+++ src/components/charts/StackedBarChart.vue
@@ -8,11 +8,17 @@ import * as d3 from "d3";
 export default {
   mounted: function() {
     var svg = d3.select(this.$el),
-      margin = { top: 35, left: 35, bottom: 0, right: 0 },
+      margin = { top: 35, left: 35, bottom: 0, right: 40 },
       width = +svg.attr("width") - margin.left - margin.right,
       height = +svg.attr("height") - margin.top - margin.bottom;
 
-    this.chartLayer = svg.append("g");
+    this.margin = { top: 35, left: 35, bottom: 0, right: 40 };
+    this.width = +svg.attr("width") - margin.left - margin.right;
+    this.height = +svg.attr("height") - margin.top - margin.bottom;
+
+    this.chartLayer = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},0)`);
 
     this.x = d3
       .scaleBand()
@@ -23,13 +29,8 @@ export default {
 
     this.xAxis = svg
       .append("g")
-      .attr("transform", `translate(0,${height - margin.bottom})`)
+      .attr("transform", `translate(${0},${height - margin.bottom})`)
       .attr("class", "x-axis");
-
-    this.yAxis = svg
-      .append("g")
-      .attr("transform", `translate(${margin.left},0)`)
-      .attr("class", "y-axis");
 
     this.drawChart(this.stackedBarChartData);
   },
@@ -53,11 +54,6 @@ export default {
 
       var speed = 0;
 
-      // data.forEach(function(d) {
-      //   d.total = d3.sum(keys, k => +d[k]);
-      //   return d;
-      // });
-
       const data = data0.map(function(d) {
         let total = d3.sum(keys, k => +d[k]);
         return { ...d, total };
@@ -65,14 +61,39 @@ export default {
 
       this.y.domain([0, d3.max(data, d => d3.sum(keys, k => +d[k]))]).nice();
 
-      this.yAxis
-        .transition()
-        .duration(speed)
-        .call(d3.axisLeft(this.y).ticks(null, "s"));
+      // this.yAxis
+      //   .transition()
+      //   .duration(speed)
+      //   .call(d3.axisLeft(this.y).ticks(null, "s"));
 
-      this.x.domain(data.map(d => d.Date));
+      // this.x.domain(data.map(d => d.Date));
+
+      var todayIndex = data.reduce((acc, cur, i) => {
+        var current = cur.Date === "TODAY" ? i : 0;
+        return acc + current;
+      }, 0);
+
+      var bandwidth = this.width / (data.length * 1.2);
+
+      this.x = d3
+        .scaleOrdinal()
+        .domain(data.map(d => d.Date))
+        .range(
+          data.map((d, i) => {
+            return i === todayIndex
+              ? this.width * ((i * 10) / 100) + bandwidth / 2
+              : i > todayIndex
+              ? this.width * ((i * 10) / 100) + bandwidth
+              : this.width * ((i * 10) / 100);
+          })
+        );
 
       this.xAxis
+        .attr(
+          "transform",
+          `translate(${this.margin.left + bandwidth / 2},${this.height -
+            this.margin.bottom})`
+        )
         .transition()
         .duration(speed)
         .call(d3.axisBottom(this.x).tickSizeOuter(0));
@@ -99,20 +120,11 @@ export default {
       bars
         .enter()
         .append("rect")
-        .attr("width", d => {
-          console.log(d);
-          return d.data.Date === "TODAY"
-            ? this.x.bandwidth() - 10
-            : this.x.bandwidth();
-        })
+        .attr("width", d => bandwidth)
         .merge(bars)
         .transition()
         .duration(speed)
-        .attr("x", d => {
-          return d.data.Date === "TODAY"
-            ? this.x(d.data.Date) + 5
-            : this.x(d.data.Date);
-        })
+        .attr("x", d => this.x(d.data.Date))
         .attr("y", d => this.y(d[1]))
         .attr("height", d => this.y(d[0]) - this.y(d[1]));
 
@@ -128,7 +140,7 @@ export default {
         .merge(text)
         .transition()
         .duration(speed)
-        .attr("x", d => this.x(d.Date) + this.x.bandwidth() / 2)
+        .attr("x", d => this.x(d.Date) + bandwidth / 2)
         .attr("y", d => this.y(d.total) - 5)
         .text(d => d.total);
     }
