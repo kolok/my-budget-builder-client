dc964cf5705c6d90bde4a97b27f16775de5cfbff
Nicolas Oudard <noudard@weborama.com>
2019-10-01 11:44:24 +0200

    fix tab-click error

--- src/views/account/Account.vue
+++ src/views/account/Account.vue
@@ -3,7 +3,7 @@
     <div v-if="userCompanyExists">
 
 
-      <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tabs v-model="activeName">
         <el-tab-pane :label='$t("Company")' name="company">
           <CompanyEdit :id="getCurrentCompany.id" />
         </el-tab-pane>
