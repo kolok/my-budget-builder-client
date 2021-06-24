93d74f78972b3775a2aea072a29ce2e4148884bb
Nicolas Oudard <nicolas.oudard@tinyclues.com>
2021-06-24 11:45:34 +0200

    Add a footer with contact and report bug

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,18 @@
         <router-view />
       </div>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from './components/common/Header.vue'
+import Footer from './components/common/Footer.vue'
 
 export default {
   components: {
     Header,
+    Footer,
   }
 }
 </script>
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="Footer" style=''>
+    <div style="margin:15px">
+      <ul class="ul-inline">
+        <li><a href="mailto:nicolas@oudard.org" target="_blank">{{ $t('Contact me') }}</a></li>
+        <li>&middot;</li>
+        <li><a href="https://github.com/kolok/my-budget-builder-client/issues" target="_blank">{{ $t('Report a bug') }}</a></li>
+      </ul>
+
+    </div>
+  </div>
+</template>
